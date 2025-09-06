cf2dd31a0afa4c8e22e91bad42ff4c8cdfad2c9e
Jourdan Callahan <jourdancallahan8@gmail.com>
2025-09-05 23:53:05 -0400

    added images

--- 12-prometheus.tf
+++ 12-prometheus.tf
@@ -1,17 +1,16 @@
-resource "helm_release" "prometheus" {
-    depends_on = [aws_eks_node_group.private-nodes, null_resource.update_kubeconfig, helm_release.ebs_csi_driver ]
-  name             = "prometheus"
-  namespace        = "observability"
-  create_namespace = true
-  chart = "kube-prometheus-stack"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  version = "65.0.0"
-  skip_crds = true 
-  values = [
-    file("./prometheus-notes/kube-prom-values.yaml")
-  ]
-}
+# resource "helm_release" "prometheus" {
+#     depends_on = [aws_eks_node_group.private-nodes, null_resource.update_kubeconfig, helm_release.ebs_csi_driver ]
+#   name             = "prometheus"
+#   namespace        = "observability"
+#   create_namespace = true
+#   chart = "kube-prometheus-stack"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   version = "65.0.0"
+#   skip_crds = true 
+#   values = [
+#     file("./prometheus-notes/kube-prom-values.yaml")
+#   ]
+# }
 
-#
 
 
